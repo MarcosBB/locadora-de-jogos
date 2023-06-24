563d1e1194167fecb97d20f368efbab2e451ddea
Heyse <luizedumariz@gmail.com>
2023-06-23 22:31:58 -0300

    esquema básico de como poderia ser
    galera vejam ai o que voces acham desse esqueleto

---
+++ Produtora.java
@@ -0,0 +1,20 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Produtora extends Empresa
+{
+    String name;
+    public List<Jogo> jogosCriados;
+    
+    public Produtora(String nome)
+    {
+        super(nome);
+        jogosCriados = new ArrayList<Jogo>();
+    }
+    
+    //FUNÇÃO IMCOMPLETA VAI DAR MERDA, PRECISAR SER AJEITADA PRA COMBINAR COM A CLASSE JOGO
+    public void CriarJogo(String nomeDoJogo, Jogo.Genero genero)
+    {
+        jogosCriados.add(new Jogo(nomeDoJogo, genero, this));
+    }
+}
